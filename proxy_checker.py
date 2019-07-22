3589a28c561589e6c58c4978d459060b26912b8f
Gautam Sharma <gautam@infobizzs.com>
2019-07-22 17:56:03 +0530

    InIt

---
+++ proxy_checker.py
@@ -0,0 +1,45 @@
+import urllib.request, socket
+import requests
+import re
+
+
+def is_bad_proxy(pip):
+    try:
+        proxy_handler = urllib.request.ProxyHandler({'https': pip})
+        opener = urllib.request.build_opener(proxy_handler)
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        urllib.request.install_opener(opener)
+        req=urllib.request.Request('https://www.yahoo.com')  # change the url address here
+        sock=urllib.request.urlopen(req)
+        print(req.full_url)
+    except urllib.request.HTTPError as e:
+        print('Error code: ', e.code)
+        return e.code
+    except Exception as detail:
+
+        print("ERROR:", detail)
+        return 1
+
+    return 0
+
+
+if __name__ == '__main__':
+
+    socket.setdefaulttimeout(10)
+    parameters = {"type": 'https', "anon": 'elite', "country": 'US'}
+
+    response = requests.get("https://www.proxy-list.download/api/v1/get", params=parameters)
+    if response.status_code == 200:
+
+        proxyList = re.findall(r'(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3}):(?:[\d]{1,5})', str(response.content))
+        print('---------- ' + str(len(proxyList)) + ' Proxy IP Found ----------')
+        working_ip =[]
+        not_woring_ip = []
+        for item in proxyList:
+            if is_bad_proxy(item):
+                not_woring_ip.append(item)
+            else:
+                working_ip.append(item)
+
+        print('Total Working IP '+ str(len(working_ip)))
+
